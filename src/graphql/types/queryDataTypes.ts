918ccd8e61618b1c7725a26ff95728e06b25f25a
pySoo <soopy368@gmail.com>
2023-10-26 07:12:34 +0900

    [feat] : query 타입 정의

---
+++ src/graphql/types/queryDataTypes.ts
@@ -0,0 +1,103 @@
+export type LocationKey = {
+  locationKey: string;
+};
+
+export type Keyword = {
+  keyword: string;
+};
+
+export type ShoppingItemType = {
+  title: string;
+  image: string;
+  link: string;
+  lprice: string;
+};
+
+export type ShoppingListType = {
+  keyword: string;
+  shoppingData: ShoppingItemType[];
+};
+
+export type ShoppingListData = {
+  getShoppingList: ShoppingListType;
+};
+
+type Metric = {
+  Metric: {
+    Value: number;
+    UnitType: number;
+    Unit: string;
+    Phase?: string;
+  };
+};
+
+type MetricValue = {
+  Value: number;
+  UnitType: number;
+  Unit: string;
+  Phase?: string;
+};
+
+type Temperature = {
+  Maximum: MetricValue;
+  Minimum: MetricValue;
+};
+
+type DayNight = {
+  Icon: number;
+  IconPhrase: string;
+  HasPrecipitation: boolean;
+  PrecipitationType: string;
+  PrecipitationProbability: number;
+  CloudCover: number;
+  ThunderstormProbability: number;
+  RainProbability: number;
+  SnowProbability: number;
+  IceProbability: number;
+};
+
+type Sun = {
+  Rise: string;
+  Set: string;
+};
+
+export type PrecipitationSummary = {
+  PastHour: Metric;
+  Past3Hours: Metric;
+  Past6Hours: Metric;
+  Past9Hours: Metric;
+  Past12Hours: Metric;
+  Past18Hours: Metric;
+  Past24Hours: Metric;
+};
+
+export type GetCurrentCondition = {
+  IsDayTime: boolean;
+  CloudCover: number;
+  UVIndex: number;
+  PrecipitationType: string;
+  PrecipitationSummary: PrecipitationSummary;
+  Temperature: Metric;
+  WeatherText: string;
+  RelativeHumidity: number;
+};
+
+export type CurrentConditionData = {
+  getCurrentCondition: GetCurrentCondition;
+};
+
+type DailyForecasts = {
+  Date: string;
+  Temperature: Temperature;
+  Day: DayNight;
+  Night: DayNight;
+  Sun: Sun;
+};
+
+export type GetFiveDaysForecast = {
+  DailyForecasts: DailyForecasts[];
+};
+
+export type FivedaysForecastData = {
+  getFiveDaysForecast: GetFiveDaysForecast;
+};
