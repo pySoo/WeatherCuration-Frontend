e58dcfd5b3ff3668b59a57b38932415ca6638f03
pySoo <soopy368@gmail.com>
2023-10-26 07:06:54 +0900

    [feat] : apolloClient 추가, uri 배포 후 변경 필요

---
+++ src/graphql/api/apolloClient.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache({}),
+  connectToDevTools: true,
+});
