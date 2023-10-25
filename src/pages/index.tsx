8f2b2b9556dd342e9d33c2cc2f9bfedbf66d94d9
pySoo <soopy368@gmail.com>
2023-10-26 07:26:24 +0900

    [feat] : graphql을 이용하여 기상 예보 불러오기 및 날씨에 대한 특이사항 저장

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
 import { ApolloError, useQuery } from '@apollo/client';
+import { useSetRecoilState } from 'recoil';
 
+import { weatherAtom } from '@/atom/weatherAtom';
 import Background from '@/components/Background';
 import Loader from '@/components/Common/Loader';
 import WeatherCuration from '@/components/WeatherCuration';
 import { POLL_INTERVAL_TIME } from '@/constants/graphql';
 import { DEFAULT_LOCATION } from '@/constants/location';
-import { CURRENT_CONDITIONS_QUERY } from '@/graphql/client/queryClient';
+import {
+  CURRENT_CONDITIONS_QUERY,
+  FIVEDAYS_FORECAST_QUERY,
+} from '@/graphql/client/queryClient';
 import {
   CurrentConditionData,
+  FivedaysForecastData,
   LocationKey,
 } from '@/graphql/types/queryDataTypes';
+import { WeatherFeatureType } from '@/types/weather';
+import { matchClothesByTemperature } from '@/utils/matchClothes';
+import { getWeatherFeature } from '@/utils/weatherFeature';
 
 export default function Main() {
   const { locationKey } = DEFAULT_LOCATION;
+  const setWeatherState = useSetRecoilState(weatherAtom);
 
   const { data: currentConditionData, error: currentConditionError } = useQuery<
     CurrentConditionData,
@@ -22,9 +32,76 @@ export default function Main() {
     pollInterval: POLL_INTERVAL_TIME,
   });
 
-  const allQuriesCompleted = currentConditionData;
+  const { data: fivedaysForecastData, error: fivedaysForecastError } = useQuery<
+    FivedaysForecastData,
+    LocationKey
+  >(FIVEDAYS_FORECAST_QUERY, {
+    variables: { locationKey },
+    pollInterval: POLL_INTERVAL_TIME,
+    onCompleted: (data) => {
+      const forecasts = data.getFiveDaysForecast.DailyForecasts;
 
-  const queryErrors = [currentConditionError].filter(Boolean);
+      // 최저 최고 온도 구하기
+      const { Maximum, Minimum } = forecasts[0].Temperature;
+
+      const maxTemperature = Math.round(Maximum.Value);
+      const minTemperature = Math.round(Minimum.Value);
+
+      const updatedTemperature = [
+        {
+          value: minTemperature,
+          keywords: matchClothesByTemperature(minTemperature),
+        },
+        {
+          value: maxTemperature,
+          keywords: matchClothesByTemperature(maxTemperature),
+        },
+      ];
+
+      // 5일간의 예보 중 특이점들을 모아서 저장
+      const weatherFeatures: WeatherFeatureType = {
+        bigTempDiff: false,
+        possibleToRain: false,
+        minTemperature: 40,
+      };
+
+      forecasts.forEach((forecast) => {
+        if (
+          Math.abs(
+            forecast.Temperature.Maximum.Value -
+              forecast.Temperature.Minimum.Value,
+          ) >= 10
+        ) {
+          weatherFeatures.bigTempDiff = true;
+        }
+
+        if (
+          forecast.Day.RainProbability >= 60 ||
+          forecast.Night.RainProbability >= 60
+        ) {
+          weatherFeatures.possibleToRain = true;
+        }
+
+        if (
+          weatherFeatures.minTemperature === null ||
+          forecast.Temperature.Minimum.Value < weatherFeatures.minTemperature
+        ) {
+          weatherFeatures.minTemperature = forecast.Temperature.Minimum.Value;
+        }
+      });
+
+      setWeatherState({
+        temperatureKeywords: updatedTemperature,
+        weatherKeywords: getWeatherFeature(weatherFeatures),
+      });
+    },
+  });
+
+  const allQuriesCompleted = currentConditionData && fivedaysForecastData;
+
+  const queryErrors = [currentConditionError, fivedaysForecastError].filter(
+    Boolean,
+  );
 
   if (queryErrors.length > 0) {
     const firstError = queryErrors[0];
@@ -33,9 +110,15 @@ export default function Main() {
 
   return (
     <div className="w-full h-screen relative">
-      <Background currentConditionData={currentConditionData}>
+      <Background
+        currentConditionData={currentConditionData}
+        fivedaysForecastData={fivedaysForecastData}
+      >
         {allQuriesCompleted ? (
-          <WeatherCuration currentConditionData={currentConditionData} />
+          <WeatherCuration
+            currentConditionData={currentConditionData}
+            fivedaysForecastData={fivedaysForecastData}
+          />
         ) : (
           <Loader />
         )}
