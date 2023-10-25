f75115f7311fe7f5c56923ea0d80a612580ea725
pySoo <soopy368@gmail.com>
2023-10-26 07:05:34 +0900

    [feat] : apollo, recoil Provider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import '@/styles/globals.css';
 
+import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { RecoilRoot } from 'recoil';
+
+import { apolloClient } from '@/graphql/api/apolloClient';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
       <Head>
         <title>Weather Curation 🧚‍♀️</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Component {...pageProps} />
-    </div>
+      <ApolloProvider client={apolloClient}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </ApolloProvider>
+    </>
   );
 }
