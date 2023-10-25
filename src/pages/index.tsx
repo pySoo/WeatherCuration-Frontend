8529b9375636aff8c2e778677302715d734b4a07
pySoo <soopy368@gmail.com>
2023-10-26 07:25:44 +0900

    [feat] : graphql을 이용하여 날씨 정보 api 불러오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,45 @@
+import { ApolloError, useQuery } from '@apollo/client';
+
 import Background from '@/components/Background';
 import Loader from '@/components/Common/Loader';
+import WeatherCuration from '@/components/WeatherCuration';
+import { POLL_INTERVAL_TIME } from '@/constants/graphql';
+import { DEFAULT_LOCATION } from '@/constants/location';
+import { CURRENT_CONDITIONS_QUERY } from '@/graphql/client/queryClient';
+import {
+  CurrentConditionData,
+  LocationKey,
+} from '@/graphql/types/queryDataTypes';
 
 export default function Main() {
+  const { locationKey } = DEFAULT_LOCATION;
+
+  const { data: currentConditionData, error: currentConditionError } = useQuery<
+    CurrentConditionData,
+    LocationKey
+  >(CURRENT_CONDITIONS_QUERY, {
+    variables: { locationKey },
+    pollInterval: POLL_INTERVAL_TIME,
+  });
+
+  const allQuriesCompleted = currentConditionData;
+
+  const queryErrors = [currentConditionError].filter(Boolean);
+
+  if (queryErrors.length > 0) {
+    const firstError = queryErrors[0];
+    throw new ApolloError(firstError!);
+  }
+
   return (
     <div className="w-full h-screen relative">
-      <Background>{true ? <div /> : <Loader />}</Background>
+      <Background currentConditionData={currentConditionData}>
+        {allQuriesCompleted ? (
+          <WeatherCuration currentConditionData={currentConditionData} />
+        ) : (
+          <Loader />
+        )}
+      </Background>
     </div>
   );
 }
