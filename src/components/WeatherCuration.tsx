618bdbe0ceca77192a41c0c25c289b03e32569f9
pySoo <soopy368@gmail.com>
2023-10-26 07:30:06 +0900

    [feat] : 날씨 영역과 추천 영역을 분리

---
+++ src/components/WeatherCuration.tsx
@@ -0,0 +1,29 @@
+import {
+  CurrentConditionData,
+  FivedaysForecastData,
+} from '@/graphql/types/queryDataTypes';
+
+import CurrentWeather from './CurrentWeather';
+import RecommendSection from './RecommendSection';
+
+type WeatherCurationProps = {
+  currentConditionData: CurrentConditionData;
+  fivedaysForecastData: FivedaysForecastData;
+};
+
+export default function WeatherCuration({
+  currentConditionData,
+  fivedaysForecastData,
+}: WeatherCurationProps) {
+  return (
+    <div className="flex justify-center items-center h-full mx-auto w-full pt-20 pb-10">
+      <div className="flex flex-col justify-center w-full h-full px-[14px] sm:px-[20px] max-w-[1200px]">
+        <CurrentWeather
+          currentConditionData={currentConditionData}
+          fivedaysForecastData={fivedaysForecastData}
+        />
+        <RecommendSection />
+      </div>
+    </div>
+  );
+}
