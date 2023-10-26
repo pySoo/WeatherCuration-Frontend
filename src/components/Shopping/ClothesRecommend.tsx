55bcb36784b3a96541e4cc07976a9d2f8392f60d
pySoo <soopy368@gmail.com>
2023-10-26 15:47:32 +0900

    [refactor] : Recommend 컴포넌트 내부에서 query 처리를 담당하도록 리팩토링

--- src/components/Shopping/ClothesRecommend.tsx
+++ src/components/Shopping/ClothesRecommend.tsx
@@ -1,20 +1,46 @@
+import { useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { weatherAtom } from '@/atom/weatherAtom';
-import { ShoppingListType } from '@/graphql/types/queryDataTypes';
+import { SHOPPING_LIST_QUERY } from '@/graphql/client/queryClient';
+import {
+  ShoppingListData,
+  ShoppingListType,
+} from '@/graphql/types/queryDataTypes';
 import { TemperatureType } from '@/types/weather';
 
 import SubTitle from '../Common/SubTitle';
 import ClothesByTemperature from './ClothesByTemperature';
 
-type ClothesRecommendProps = {
-  clothesList: ShoppingListType[];
-};
+export default function ClothesRecommend() {
+  const [clothesList, setClothesList] = useState<ShoppingListType[]>([]);
 
-export default function ClothesRecommend({
-  clothesList,
-}: ClothesRecommendProps) {
   const weatherState = useRecoilValue(weatherAtom);
+
+  const clothesKeywordsByTemperature = weatherState.temperatureKeywords
+    .map((item) => item.keywords)
+    .flat();
+
+  const client = useApolloClient();
+
+  useEffect(() => {
+    Promise.all(
+      clothesKeywordsByTemperature.map((keyword) =>
+        client.query<ShoppingListData>({
+          query: SHOPPING_LIST_QUERY,
+          variables: { keyword },
+        }),
+      ),
+    ).then((results) => {
+      setClothesList(
+        results.map((result) => {
+          const { keyword, shoppingData } = result.data.getShoppingList;
+          return { keyword, shoppingData };
+        }),
+      );
+    });
+  }, [weatherState.temperatureKeywords]);
 
   return (
     <div className="flex flex-col">
