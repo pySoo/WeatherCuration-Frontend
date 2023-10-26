7a5a657c934e1ee4724fab5e714aa6c5fb06c8d8
pySoo <soopy368@gmail.com>
2023-10-26 15:48:18 +0900

    [feat] : shopping api data를 불러오는 동안 loader를 보여주도록 수정

---
+++ src/components/Common/ShoppingLoader.tsx
@@ -0,0 +1,21 @@
+import { RotatingSquare } from 'react-loader-spinner';
+
+export default function ShoppingLoader() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <RotatingSquare
+        height="100"
+        width="100"
+        color="#4377fd"
+        ariaLabel="rotating-square-loading"
+        strokeWidth="4"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+      <p className="tracking-tight text-gray-300">
+        요즘 주목받는 상품을 불러오고 있어요<span className="ml-2">👀</span>
+      </p>
+    </div>
+  );
+}
--- src/components/Shopping/ShoppingList.tsx
+++ src/components/Shopping/ShoppingList.tsx
@@ -1,5 +1,6 @@
 import { ShoppingListType } from '@/graphql/types/queryDataTypes';
 
+import ShoppingLoader from '../Common/ShoppingLoader';
 import ShoppingItem from './ShoppingItem';
 
 type ShoppingListProps = {
@@ -18,17 +19,20 @@ export default function ShoppingList({
   )?.shoppingData;
 
   return (
-    <div className="grid-item pt-4 pb-3 h-[314px]">
-      <div className=" flex w-full overflow-scroll scrollbar-hide h-[284px] justify-center">
-        <div className="flex items-center gap-3 snap-x snap-mandatory px-3.5">
-          {shoppingItem &&
+    <div className="grid-item pt-4 pb-3 h-[314px] ">
+      <div className=" flex w-full overflow-scroll scrollbar-hide h-[284px]">
+        <div className="w-full flex items-center gap-3 snap-x snap-mandatory px-3.5">
+          {shoppingItem ? (
             shoppingItem.map((item) => (
               <ShoppingItem
                 key={item.title}
                 shoppingItem={item}
                 isPriority={isPriority}
               />
-            ))}
+            ))
+          ) : (
+            <ShoppingLoader />
+          )}
         </div>
       </div>
     </div>
