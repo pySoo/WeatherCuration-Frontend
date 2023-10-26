9655ef7c8c89cbe46778cfbc6beeed9ed2b9dbcc
pySoo <soopy368@gmail.com>
2023-10-27 08:10:56 +0900

    [feat] : 상품 변경

--- src/utils/matchClothes.ts
+++ src/utils/matchClothes.ts
@@ -7,12 +7,12 @@ export const matchClothesByTemperature = (temperature: number) => {
     return ['트렌치코트', '야상', '자켓'];
   } else if (temperature < 17) {
     return ['니트', '가디건', '맨투맨'];
-  } else if (temperature < 20) {
+  } else if (temperature < 21) {
     return ['후드티', '바람막이', '슬랙스'];
-  } else if (temperature < 23) {
-    return ['셔츠', '블라우스', '면 바지'];
+  } else if (temperature < 25) {
+    return ['얇은 셔츠', '블라우스', '청바지'];
   } else if (temperature < 28) {
-    return ['얇은 셔츠', '셔츠'];
+    return ['얇은 셔츠', '반바지'];
   } else {
     return ['민소매', '린넨'];
   }
